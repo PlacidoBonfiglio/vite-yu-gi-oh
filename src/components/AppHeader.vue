c941f6db9d90b7e5d5a1b87e6aba15d9d0a6c21a
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-18 15:38:49 +0200

    added navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,10 +9,18 @@
 </script>
 
 <template>
-    <h1>
-        HEADER
-    </h1>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand fs-2" href="#">
+                <img src="../assets/img/Yu-Gi-Oh!.png" alt="Yu-Gi-Oh logo" class="img-fluid">
+                Yu-Gi-Oh Api
+            </a>
+        </div>
+    </nav>
 </template>
 
 <style lang="scss" scoped>
+    img {
+        height: 25px;
+    }
 </style>
