4f664ff184b6bc393adf57b0349958459cfa4626
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-18 18:51:06 +0200

    added margin right to img in navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,7 +12,7 @@
     <nav class="navbar bg-body-tertiary mb-5">
         <div class="container-fluid">
             <a class="navbar-brand fs-2" href="#">
-                <img src="../assets/img/Yu-Gi-Oh!.png" alt="Yu-Gi-Oh logo" class="img-fluid">
+                <img src="../assets/img/Yu-Gi-Oh!.png" alt="Yu-Gi-Oh logo" class="img-fluid me-3">
                 Yu-Gi-Oh Api
             </a>
         </div>
